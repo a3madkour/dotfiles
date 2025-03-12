b9a46ce6cf3c77bc7fce7d30e06ad22956208a19
Abdelrahman Madkour <a3madkour@gmail.com>
2025-03-12 09:43:52 -0400

    Fixed some eglot issues

--- emacs-configs/custom/init.el
+++ emacs-configs/custom/init.el
@@ -21,20 +21,22 @@
 
 (defvar bootstrap-version)
 (let ((bootstrap-file
-       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
-      (bootstrap-version 5))
+       (expand-file-name
+		"straight/repos/straight.el/bootstrap.el"
+		(or (bound-and-true-p straight-base-dir) user-emacs-directory)))
+      (bootstrap-version 7))
   (unless (file-exists-p bootstrap-file)
-    (with-current-buffer
-        (url-retrieve-synchronously
-         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
-         'silent 'inhibit-cookies)
+    (with-current-buffer (url-retrieve-synchronously
+						  "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+						  'silent 'inhibit-cookies)
       (goto-char (point-max))
       (eval-print-last-sexp)))
   (load bootstrap-file nil 'nomessage))
 
 (straight-use-package 'use-package)
 (setq straight-use-package-by-default t)
-
+(setq straight-built-in-pseudo-packages
+	  '(emacs nadvice python image-mode project flymake))
 (use-package org)
 
 (org-babel-load-file "~/emacs-configs/custom/config.org")
