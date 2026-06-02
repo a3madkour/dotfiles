d5205fff1e74212b03d239aef2ef5b86f889cb3d
Abdelrahman Madkour <a3madkour@gmail.com>
2026-06-02 15:09:21 -0700

    refactor(d.2): multi-filter --doc-p delegates to keywords API + std header

--- emacs-configs/custom/lisp/a3madkour-publish-multi-filter.el
+++ emacs-configs/custom/lisp/a3madkour-publish-multi-filter.el
@@ -1,17 +1,22 @@
 ;;; a3madkour-publish-multi-filter.el --- D.2 multi-export visibility + vocab filter -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
 ;; Shared filter module for the D.2 multi-target export pipeline.
 ;; Provides: opt-in detection, visibility-tag filter, D.1 vocab translation.
-(require 'org)
-(require 'org-element)
+
+;;; Code:
+
+(require 'a3madkour-publish-keywords)
 
 (defconst a3madkour-pub-multi-filter--opt-in-keyword "MULTI_EXPORT"
   "Org buffer-keyword that opts a document into the multi-export pipeline.")
 
 (defun a3madkour-pub-multi-filter--doc-p ()
   "Return non-nil iff current buffer carries `#+multi_export: t'."
-  (let ((value (cadar (org-collect-keywords
-                       (list a3madkour-pub-multi-filter--opt-in-keyword)))))
-    (and value (string= (downcase (string-trim value)) "t"))))
+  (a3madkour-pub-keywords/boolean-p
+   (a3madkour-pub-keywords/extract
+    a3madkour-pub-multi-filter--opt-in-keyword)))
 
 (provide 'a3madkour-publish-multi-filter)
 ;;; a3madkour-publish-multi-filter.el ends here
